a143dac62f574486e05b797a6ea2ca8b09bcf998
Jackong <JackongC@gmail.com>
2015-11-13 13:26:21 +0800

    add dependencies to package.json

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,57 +1,39 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+'use strict'
+const yeoman = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
+  init: function() {
     this.log(yosay(
       'Welcome to the impeccable ' + chalk.red('Reactapp') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
+    ))
+    this.appname = this.appname.replace(/\s/g, '-')
   },
-
   writing: {
-    app: function () {
-      this.fs.copy(
+    app: function() {
+      this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+        this.destinationPath('package.json'),
+        {
+            appname: this.appname
+        }
+      )
     },
 
-    projectfiles: function () {
+    projectfiles: function() {
       this.fs.copy(
         this.templatePath('editorconfig'),
         this.destinationPath('.editorconfig')
-      );
+      )
       this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
+        this.templatePath('.eslintrc'),
+        this.destinationPath('.eslintrc')
+      )
     }
   },
 
-  install: function () {
-    this.installDependencies();
+  install: function() {
+    this.installDependencies()
   }
-});
+})
