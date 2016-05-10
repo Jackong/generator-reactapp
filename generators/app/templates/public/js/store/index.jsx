134339c829a6efc71ff06f48970b5b40eaf26744
Jackong <daisy@Jackong.local>
2016-05-10 11:58:10 +0800

    use hashHistory from react-router

--- generators/app/templates/public/js/store/index.jsx
+++ generators/app/templates/public/js/store/index.jsx
@@ -1,12 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import {useRouterHistory} from 'react-router'
-import {createHashHistory} from 'history'
+import {hashHistory} from 'react-router'
 import {routerMiddleware} from 'react-router-redux'
 
 import reducers from '../reducers'
-
-export const hashHistory = useRouterHistory(createHashHistory)({queryKey: false})
 
 let tools = []
 let middlewares = [thunk, routerMiddleware(hashHistory)]
