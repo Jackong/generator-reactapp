9a4ed1911f1113eb433cbf030f42c3613e200d34
Jackong <daisy@Jackong.local>
2016-08-19 21:12:27 +0800

    release 1.4.7

--- generators/app/templates/src/services/user.js
+++ generators/app/templates/src/services/user.js
@@ -3,4 +3,5 @@ import api from './api';
 export const getUser = ({ id }) => api
   .custom('user')
   .get({ id })
-  .then(res => res.body().data());
+  .then(res => res.body().data())
+  .catch(error => ({ error }));
