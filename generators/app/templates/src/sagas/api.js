172f4cb118008498b323d79246d56aea8827292e
Jackong <daisy@Jackong.local>
2016-09-09 11:55:50 +0800

    update README and tests

--- generators/app/templates/src/sagas/api.js
+++ generators/app/templates/src/sagas/api.js
@@ -5,37 +5,42 @@ import { normalize, Schema } from 'normalizr';
 import { action, ERROR } from '../actions';
 import { SUCCESS } from '../constants/code';
 
+export function* modelize(data, schemas) {
+  const { result, entities } = normalize(data, schemas);
+  Object.keys(schemas).forEach(key => {
+    let ids = result[key];
+    const schema = schemas[key];
+    let schemaKey = key;
+    let Model = null;
+    if (schema instanceof Schema) {
+      schemaKey = schema.getKey();
+      ids = [ids];
+      Model = schema.getMeta('model');
+    } else {
+      Model = schema.getItemSchema().getMeta('model');
+    }
+    const schemaEntities = entities[schemaKey];
+    ids.forEach(id => {
+      schemaEntities[id] = new Model(schemaEntities[id]);
+    });
+    entities[schemaKey] = schemaEntities;
+  });
+  return { result, entities };
+}
+
 export function* callAPI({ payload, meta: { api, types, schemas } }) {
   try {
     const res = yield call(api, payload);
     const data = res.body().data();
     if (data.code !== SUCCESS) {
-      yield put(action(types.FAILURE, { request: payload, response: data }));
+      yield put(action(types.FAILURE, payload, data));
       return;
     }
     if (!schemas || Object.keys(schemas).length <= 0) {
-      yield put({ type: types.SUCCESS, payload });
+      yield put(action(types.SUCCESS, payload, data));
       return;
     }
-    const { result, entities } = normalize(data, schemas);
-    Object.keys(schemas).forEach(key => {
-      let ids = result[key];
-      const schema = schemas[key];
-      let schemaKey = key;
-      let Model = null;
-      if (schema instanceof Schema) {
-        schemaKey = schema.getKey();
-        ids = [ids];
-        Model = schema.getMeta('model');
-      } else {
-        Model = schema.getItemSchema().getMeta('model');
-      }
-      const schemaEntities = entities[schemaKey];
-      ids.forEach(id => {
-        schemaEntities[id] = new Model(schemaEntities[id]);
-      });
-      entities[schemaKey] = schemaEntities;
-    });
+    const { result, entities } = yield call(modelize, data, schemas);
     yield put(action(types.SUCCESS, { result, entities }));
   } catch (error) {
     yield put(action(ERROR.CATCH, error));
