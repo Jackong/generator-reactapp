08a5347f3ad034db446ec83cecf3cd4df8fd3c18
Jackong <daisy@Jackong.local>
2016-10-29 23:17:07 +0800

    add sub-generator: store

---
+++ generators/store/templates/store.js
@@ -0,0 +1,43 @@
+import { observable, action } from 'mobx';
+
+import { getAll, add, update, remove } from '../api/<%= name %>';
+
+export default class <%= className %> {
+  @observable <%= plural %> = [];
+
+  @action getAll = () => {
+    getAll()
+    .then((entities) => {
+      this.<%= plural %> = entities;
+    });
+  }
+
+  @action add = (<%= name %>) => {
+    add(<%= name %>)
+    .then((entity) => {
+      this.<%= plural %>.push(entity);
+    });
+  }
+
+  @action update = (origin, changed) => {
+    const index = this.<%= plural %>.indexOf(origin);
+    if (index < 0) {
+      return;
+    }
+    update(changed)
+    .then(() => {
+      this.<%= plural %>[index] = { ...origin, ...changed };
+    });
+  }
+
+  @action remove = (<%= name %>) => {
+    const index = this.<%= plural %>.indexOf(<%= name %>);
+    if (index < 0) {
+      return;
+    }
+    remove(<%= name %>)
+    .then(() => {
+      this.<%= plural %>.splice(index, 1);
+    });
+  }
+}
